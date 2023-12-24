67b413bd9150646dc54ca615625bcd657640f638
AhsenRiaz <ahsenriaz2001@gmail.com>
2023-12-24 18:04:57 +0500

    feat: polymorphism

---
+++ 3_polymorphism/1_Animal.py
@@ -0,0 +1,27 @@
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        print(f"{self.name} is eating {food}")
+
+
+class Dog(Animal):
+    def fetch(self, thing):
+        print(f"{self.name} goes after the {thing}")
+
+    def show_affection(self):
+        print(f"{self.name} wags tail")
+
+
+class Cat(Animal):
+    def swatstring(self):
+        print(f"{self.name} shreds the string")
+
+    def show_affection(self):
+        print(f"{self.name} purrs")
+
+
+for a in (Dog("Rover"), Cat('Fluffy')):
+    a.show_affection()
